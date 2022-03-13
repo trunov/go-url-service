adea3abdcf709b768cf5031f3af53d5751a11582
Kirill Trunov <kirill.trunov@visioncraft.ee>
2022-03-13 16:35:43 +0200

    add Config for ServerAddress and BaseUrl

--- cmd/shortener/server.go
+++ cmd/shortener/server.go
@@ -4,21 +4,33 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/caarlos0/env/v6"
 	"github.com/go-chi/chi/v5"
 	"github.com/trunov/go-url-service/internal/app/handlers"
 	"github.com/trunov/go-url-service/internal/app/storage"
 )
 
+type Config struct {
+	ServerAddress string `env:"SERVER_ADDRESS"`
+	BaseUrl string `env:"BASE_URL"`
+}
+
 func StartServer() {
 	urls := make(map[string]string, 10)
+	var cfg Config
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	s := storage.NewStorage(urls)
 
 	h := handlers.NewHandlers(s)
 
 	r := chi.NewRouter()
 	r.Post("/", h.ShortenHandler)
-	r.Post("/api/shorten", h.NewShortenHandler)
+	r.Post(cfg.BaseUrl, h.NewShortenHandler)
 	r.Get("/{id}", h.RedirectHandler)
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(cfg.ServerAddress, r))
 }
