d27957dadb28410e8d6bd0c1f0c4ec08e0072f0e
Kirill Trunov <kirill.trunov@visioncraft.ee>
2022-04-15 22:40:27 +0300

    remove devault env from baseURL

--- cmd/shortener/server.go
+++ cmd/shortener/server.go
@@ -13,7 +13,7 @@ import (
 
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
-	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	BaseURL       string `env:"BASE_URL"`
 }
 
 // сделать конфиг фолдер и там инициализацию проводить ?!
