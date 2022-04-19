7c1eb054a5aa4d4e11afae05f69c9cb9ff9ca0a7
Kirill Trunov <kirill.trunov@visioncraft.ee>
2022-04-19 10:55:20 +0300

    change env

--- cmd/shortener/server.go
+++ cmd/shortener/server.go
@@ -14,7 +14,7 @@ import (
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
 	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	FileStorage   string `env:"FILE_STORAGE"`
+	FileStorage   string `env:"FILE_STORAGE_PATH"`
 }
 
 func StartServer() {
